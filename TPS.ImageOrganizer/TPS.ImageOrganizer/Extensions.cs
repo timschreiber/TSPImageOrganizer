f62323bc0d46aa6301e7b7defb5e855e267620be
Tim Schreiber <timschreiber@users.noreply.github.com>
2016-03-23 12:17:15 -0400

    added extensions class

---
+++ TPS.ImageOrganizer/TPS.ImageOrganizer/Extensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TPS.ImageOrganizer
+{
+    public static class Extensions
+    {
+        public static IEnumerable<FileInfo> GetFilesByExtensions(this DirectoryInfo dir, params string[] extensions)
+        {
+            if (extensions == null || extensions.Length == 0)
+                return dir.GetFiles();
+            var files = dir.EnumerateFiles();
+            return files.Where(f => extensions.Select(x => x.ToLower()).Contains(f.Extension.ToLower()));
+        }
+    }
+}
